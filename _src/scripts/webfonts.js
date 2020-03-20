b750c8c5c64b23b62204501e0d6345b588eeb456
christophe le drean <christophe@rouge-rose.net>
2020-03-20 21:07:17 +0100

    Adaptation script de copie des polices

--- _src/scripts/webfonts.js
+++ _src/scripts/webfonts.js
@@ -1,10 +1,12 @@
 var fonts = {
-	barlow: ['_src/webfonts/barlow/fonts/woff2/Barlow-Regular.woff2', '_src/webfonts/barlow/fonts/woff2/Barlow-Italic.woff2', '_src/webfonts/barlow/fonts/woff2/Barlow-Bold.woff2', '_src/webfonts/barlow/fonts/woff2/Barlow-BoldItalic.woff2'],
+	barlow_woff2: ['_src/webfonts/barlow/fonts/woff2/Barlow-Regular.woff2', '_src/webfonts/barlow/fonts/woff2/Barlow-Italic.woff2', '_src/webfonts/barlow/fonts/woff2/Barlow-Bold.woff2', '_src/webfonts/barlow/fonts/woff2/Barlow-BoldItalic.woff2'],
+	barlow_woff: ['_src/webfonts/barlow/fonts/woff/Barlow-Regular.woff', '_src/webfonts/barlow/fonts/woff/Barlow-Italic.woff', '_src/webfonts/barlow/fonts/woff/Barlow-Bold.woff', '_src/webfonts/barlow/fonts/woff/Barlow-BoldItalic.woff'],
 }
 
 const cpy = require('cpy');
+var barlow = fonts.barlow_woff2.concat(fonts.barlow_woff);
 
 (async () => {
-	await cpy(fonts.barlow, 'dist/webfonts/');
+	await cpy(barlow, 'dist/webfonts/');
 	// console.log('Files copied!');
 })();
