ac3b70e68ed57f6963dd3cad9b433d717f67cdae
christophe le drean <christophe@rouge-rose.net>
2020-03-31 15:10:24 +0200

    Page d'introduction
    - le script affiche ou pas une page d'introduction
    - animation du texte via css

--- _src/javascript/a2ta.js
+++ _src/javascript/a2ta.js
@@ -1,10 +1,11 @@
-
-var a2ta = {
-	body: null,
+var A2ta = {
 	init: function() {
-		this.body = $("body"),
-		this.menu(),
-		this.loading();
+		(this.body = $("body")),
+			(this.loadingContainer = $("#loading")),
+			this.body.addClass("is-page-ready"),
+			this.menu(),
+			this.loading(),
+			this.loadingEvents();
 	},
 
 	menu: function() {
@@ -15,28 +16,26 @@ var a2ta = {
 	},
 
 	loading: function() {
-		var $loadingContainer = $("#loading"),
-				loadingContainer = $loadingContainer[0];
-
-		loadingContainer.addEventListener('swiped-up', hideLoading);
-		loadingContainer.addEventListener('click', hideLoading);
-
-		this.body.addClass("is-page-ready");
-
-
-		function hideLoading() {
-			$loadingContainer.addClass("is-hidden");
-
-			// Supprimer body.is-locked
-			event.currentTarget.parentNode.classList.remove("is-locked");
-
-			loadingContainer.removeEventListener('swiped-up', hideLoading);
-			loadingContainer.removeEventListener('click', hideLoading);
+		if (this.loadingContainer.hasClass("is-visible")) {
+			this.body.addClass("is-locked");
 		}
 	},
-}
 
+	loadingEvents: function() {
+		var container = this.loadingContainer[0];
+		container.addEventListener("swiped-up", hideLoading);
+		container.addEventListener("click", hideLoading);
+
+		function hideLoading(event) {
+			container.removeEventListener("swiped-up", hideLoading);
+			container.removeEventListener("click", hideLoading);
+			container.classList.remove("is-visible");
+			container.classList.add("is-hidden");
+			container.parentNode.classList.remove("is-locked");
+		}
+	}
+};
 
 $(function() {
-	a2ta.init();
+	A2ta.init();
 });
