f913947009d0692a0533d0936cd28bf82efee2ee
christophe le drean <christophe@rouge-rose.net>
2020-04-19 12:34:23 +0200

    Réunir le js relatif à la carte dans le même objet

--- _src/javascript/a2ta_map.js
+++ _src/javascript/a2ta_map.js
@@ -1,16 +1,155 @@
 var A2taMap = {
-	jqCarte: {},
-	objCarte: {},
 	self: {},
+	carte: {},
+	criteresLabelRecherche: [],
+	criteresIdRecherche: [],
+	containerlistCriteres: {},
+	inputCarteRechercheLibre: {},
+	listAcResponse: {},
 	init: function (mapObject) {
 		self = this;
-		self.objCarte = mapObject;
+		self.carte = mapObject;
 		self.setZoomControl();
+		self.containerlistCriteres = $("#listCriteres");
+		self.inputCarteRechercheLibre = $("#input-carte-recherche-libre");
+		self.listAcResponse = $("#listAutocompleteResponse");
+		$(self.ready);
+	},
+
+	ready: function () {
+		self.rechercheLibreNoResult();
 	},
 
 	setZoomControl: function () {
-		self.objCarte.options.zoomControl = false;
-		L.control.zoom({ position: "bottomleft" }).addTo(self.objCarte);
+		self.carte.options.zoomControl = false;
+		L.control.zoom({ position: "bottomleft" }).addTo(self.carte);
+	},
+
+	rechercheLibreCb: function (event, ui) {
+		var critereValue = ui.item.value,
+			critereLabel = ui.item.label,
+			dejaPresent = self.criteresIdRecherche.indexOf(critereValue);
+
+		if (dejaPresent == -1) {
+			self.criteresLabelRecherche.push(critereLabel);
+			self.criteresIdRecherche.push(critereValue);
+			self.afficherCriteres();
+		}
+
+		this.value = "";
+
+		// empêcher de poursuivre avec le fonctionnement
+		// par défaut sur l'événement select
+		return false;
+	},
+
+	afficherCriteres: function () {
+		var listCriteres = '<ul class="o-list-bare c-carte__criteres">';
+
+		for (var i = 0; i < self.criteresLabelRecherche.length; i++) {
+			listCriteres +=
+				'<li class="o-liste-bare__item c-carte__critere">' +
+				'<span class="c-carte__critere-label">' +
+				self.criteresLabelRecherche[i] +
+				"</span>" +
+				'<button id="critereDelete' +
+				i +
+				'" class="c-btn c-carte__critere-btn"></button></li>';
+		}
+
+		listCriteres += "</ul>";
+		self.containerlistCriteres.html(listCriteres);
+		self.chargerGeoPoints();
+	},
+
+	chargerGeoPoints: function () {
+		self.carte.removeAllMarkers();
+		var parametres = { id_association: [], id_mot: [], ville: [], limit: 500 };
+
+		// Construire la requête. Les critères sont stockés ainsi :
+		// "id_nomObjet:identifiant numérique objet".
+		for (var i = 0; i < self.criteresIdRecherche.length; i++) {
+			var p = self.criteresIdRecherche[i].split(":");
+
+			for (var property in parametres) {
+				var k = p[0],
+					l = parametres[k].length;
+
+				if (property === k) {
+					parametres[k][l] = p[1];
+				}
+			}
+		}
+
+		var query = $.param(parametres);
+
+		// Récupérer les id_association à partir des critères de recherche
+		$.getJSON("http.api/collectionjson/associations" + "?" + query)
+			.done(function (json) {
+				var associationsList = { id_association: [] };
+
+				$.each(json.collection.items, function (key, item) {
+					associationsList.id_association[key] = item.data[0].value;
+				});
+				// Récupérer les infos de géolocalisation depuis le squelette
+				// json/gis_associations_env
+				var query = $.param(associationsList);
+				$.getJSON(
+					"?page=gis_json&objets=associations_env&limit=500&" + query
+				).done(function (json) {
+					self.parseJson(json);
+				});
+			})
+			.fail(function (jqxhr, textStatus, error) {
+				var message = getMessageInfoNoResult();
+				self.containerlistCriteres.append(message);
+			});
+	},
+
+	rechercheLibreNoResult: function () {
+		// Calculer la largeur du container.
+		// TODO: Vérifier la largeur sur le resize ou autres événements ?
+		var width = self.inputCarteRechercheLibre.outerWidth();
+
+		self.inputCarteRechercheLibre.on("autocompleteresponse", function (
+			event,
+			ui
+		) {
+			if (!ui.content.length) {
+				var inputVal = this.value;
+
+				if (inputVal) {
+					self.listAcResponse.css("width", width + "px");
+					var message = self.getMessageNoResult(inputVal);
+					self.listAcResponse.html(message);
+					$(this).on("blur", self.cancelMessageNoResult);
+					// $(this).off("blur", self.cancelMessageNoResult);
+				}
+			} else {
+				self.cancelMessageNoResult();
+			}
+		});
+	},
+
+	getMessageNoResult: function (value) {
+		var message = "",
+			messageT = "";
+
+		if (value) {
+			messageT = "Aucun résultat pour « " + value + " »";
+		} else {
+			messageT = "Aucun résultat";
+		}
+
+		message = '<div id="infoNoResult" class="c-message c-message--info">';
+		message += messageT;
+		message += "</div>";
+
+		return message;
+	},
+
+	cancelMessageNoResult: function () {
+		$("#infoNoResult").css("display", "none");
 	},
 
 	// Utiliser une fonction spécifique qui reprend l'essentiel
@@ -18,8 +157,9 @@ var A2taMap = {
 	// de zoom, difficile à maîtriser et qui sont ajoutées par GIS
 	// dans centerAndZoom
 	parseJson: function (json) {
-		var map = self.objCarte;
+		var map = self.carte;
 		var markers = [];
+		var autres = { type: "FeatureCollection", features: [] };
 		map.markerCluster = L.markerClusterGroup(map.options.clusterOptions).addTo(
 			map
 		);
@@ -35,8 +175,11 @@ var A2taMap = {
 				map.setGeoJsonFeatureIcon(feature, marker);
 				map.setGeoJsonFeaturePopup(feature, marker);
 				marker.feature = feature;
-				marker.id = feature.id;
+				// marker.id = feature.id;
 				markers.push(marker);
+			} else {
+				autres.features.push(feature);
+				console.log(autres);
 			}
 		});
 		map.markerCluster.addLayers(markers);
