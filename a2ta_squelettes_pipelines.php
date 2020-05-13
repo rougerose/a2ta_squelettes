18b767abe6d4fdfc8ab372fd507f42f9958826e2
christophe le drean <christophe@rouge-rose.net>
2020-05-13 12:59:44 +0200

    Debug

--- a2ta_squelettes_pipelines.php
+++ a2ta_squelettes_pipelines.php
@@ -15,7 +15,7 @@ function a2ta_squelettes_recuperer_fond($flux) {
     // leaflet-sidebar
     $js = spip_file_get_contents(find_in_path('dist/js/L.Control.Sidebar.js'));
     // leaflet-spin
-    $js .= "\n".spip_file_get_contents(find_in_path('dist/js/leaflet.spin.js'));
+    //$js .= "\n".spip_file_get_contents(find_in_path('dist/js/leaflet.spin.js'));
     // leaflet-easybutton
     $js .= "\n".spip_file_get_contents(find_in_path('dist/js/easy-button.js'));
     $flux['data']['texte'] .= "\n".$js;
